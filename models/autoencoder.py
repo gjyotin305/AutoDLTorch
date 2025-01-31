037e7ebc88553e63e6b5e2be968e86c1f4d8ec09
Jyotin Goel <b22ai063@iitj.ac.in>
2025-01-31 12:03:17 +0530

    Simple Auto Encoders | Normal
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -25,7 +25,28 @@ class SimpleAutoEncoder(nn.Module):
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        input_dim,
+        hidden_dim,
+        activation_fn,
+        output_dim,
+        n_layers
+    ):
         super().__init__(AutoEncoder, self)
-        
+        self.in_model = nn.Linear(input_dim, hidden_dim)
+        self.act_fn = activation_fn
+        self.interim_model = nn.Sequential(*[
+            nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim),
+                self.act_fn
+            ) for _ in range(n_layers)]
+        )
+        self.out_model = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        out = self.in_model(x)
+        out = self.interim_model(out)
+        out = self.out_model(x)
+        return out
 
