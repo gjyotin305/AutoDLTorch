ca169f7daf5176470ed0731a63ffebf6c2279d37
Jyotin Goel <b22ai063@iitj.ac.in>
2025-02-19 16:31:09 +0530

    Structure Change
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- models/convolutional/convolution.py
+++ models/convolutional/convolution.py
@@ -56,3 +56,39 @@ class CNN(nn.Module):
         # out = self.conv_blocks(out)
         # out = self.fc(out)
         # return out
+
+
+class TinyResNet(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class TinyUNet(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class TinyInceptionNet(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class MobileNet(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class TinyDenseNet(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+
+
+class ConvNext(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
