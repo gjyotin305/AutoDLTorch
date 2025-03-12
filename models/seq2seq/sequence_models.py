76637b712ae8b4bc12966cb168a6857ce273530e
Jyotin Goel <b22ai063@iitj.ac.in>
2025-03-13 03:15:19 +0530

    Gated Architecture
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- models/seq2seq/sequence_models.py
+++ models/seq2seq/sequence_models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
@@ -13,6 +14,7 @@ class ElmanNetwork(nn.Module):
     def forward(self, x):
         # ht = ReLU(WxhXt + WhhHt-1 + Bh)
         h = x.new_zeros(x.size(0), self.h2y.weight.size(1))
+        
         for t in range(input.size(1)):
             h = F.relu(self.x2h(x[:, t]) + self.h2h(h))
         
@@ -24,7 +26,40 @@ class RNNArch(nn.Module):
         raise NotImplementedError
 
 
-class LSTMArch(nn.Module):
-    def __init__(self):
-        super().__init__()
-        raise NotImplementedError
+class GatedArch(nn.Module):
+    def __init__(self, input_dim, recurrent_dim, output_dim):
+        super(GatedArch, self).__init__()
+        self.input_dim = input_dim
+        self.recurrent_dim = recurrent_dim
+        self.output_dim = output_dim
+        self._init_layers()
+        
+    def _init_layers(self):
+        self.x2h = nn.Linear(self.input_dim, self.recurrent_dim)
+        self.h2h = nn.Linear(self.recurrent_dim, self.recurrent_dim, bias=False)
+        self.x2z = nn.Linear(self.input_dim, self.recurrent_dim)
+        self.h2z = nn.Linear(self.recurrent_dim, self.recurrent_dim, bias=False)
+        self.h2y = nn.Linear(self.recurrent_dim, self.output_dim)
+    
+    def forward(self, x):
+        """    
+        Process:
+
+        - Initial Recurrent Update:
+        `Ht_candidate = ReLU(Wxh @ Xt + Whh @ Ht_1 + bh)`
+        - Full Update:
+        The same equation as the initial recurrent update (for clarity).
+        - Forget Gate:
+        `Zt = ActFn(Wxz @ Xt + Whz @ Ht_1 + bz)`
+        - Gated Recurrent Update:
+        `Ht = Zt * Ht_1 + (1 - Zt) * Ht_candidate`
+        """
+
+        h0 = x.new_zeros(x.size(0), self.h2y.weight.size(1))
+
+        for t in range(x.size(1)):
+            z = torch.sigmoid(self.x2z(x[:, t] + self.h2z(h0)))
+            hb = F.relu(self.x2h(x[:, t]) + self.h2h(h0))
+            h0 = z*h0 + (1-z)*hb
+        
+        return self.h2y(h0)
