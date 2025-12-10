d4012292398c95496b70d2e61292125307a872b6
gjyotin305 <b22ai063@iitj.ac.in>
2025-12-11 01:47:15 +0530

    Implementation Comparable to HF
    Signed-off-by: gjyotin305 <b22ai063@iitj.ac.in>

--- efficient_sft/model.py
+++ efficient_sft/model.py
@@ -16,37 +16,43 @@ get_current_device = torch.cuda.current_device
 #     tensor
 
 
-class RotaryEmbedding:
-    def __init__(self) -> None:
-        pass
+# class RotaryEmbedding:
+#     def __init__(self) -> None:
+#         pass
 
-    def _precompute_rotary_embeddings(self, seq_len, head_dim, base=10000, device=None):
-        device='cpu'
-        channel_range = torch.arange(0, head_dim, 2, dtype=torch.float32, device=device)
-        inv_freq = 1.0 / (base ** (channel_range / head_dim))
-        # stride the time steps
-        t = torch.arange(seq_len, dtype=torch.float32, device=device)
-        # calculate the rotation frequencies at each (time, channel) pair
-        freqs = torch.outer(t, inv_freq)
-        cos, sin = freqs.cos(), freqs.sin()
-        cos, sin = cos.bfloat16(), sin.bfloat16() # keep them in bfloat16
-        cos, sin = cos[None, :, None, :], sin[None, :, None, :] # add batch and head dims for later broadcasting
-        return cos, sin
+#     def _precompute_rotary_embeddings(self, seq_len, head_dim, base=10000, device=None):
+#         device='cpu'
+#         channel_range = torch.arange(0, head_dim, 2, dtype=torch.float32, device=device)
+#         inv_freq = 1.0 / (base ** (channel_range / head_dim))
+#         # stride the time steps
+#         t = torch.arange(seq_len, dtype=torch.float32, device=device)
+#         # calculate the rotation frequencies at each (time, channel) pair
+#         freqs = torch.outer(t, inv_freq)
+#         cos, sin = freqs.cos(), freqs.sin()
+#         cos, sin = cos.bfloat16(), sin.bfloat16() # keep them in bfloat16
+#         cos, sin = cos[None, :, None, :], sin[None, :, None, :] # add batch and head dims for later broadcasting
+#         return cos, sin
 
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
 
-def apply_rotary_emb(x, cos, sin):
-    assert x.ndim == 4  # multihead attention
-    d = x.shape[3] // 2
-    cos = rearrange(cos, 'a b c d -> a c b d')
-    sin = rearrange(sin, 'a b c d -> a c b d')
-    x1, x2 = x[..., :d], x[..., d:] # split up last time into two halves
-    x1, x2 = x1.to(cos.device), x2.to(cos.device)
-    # print(x1.shape, cos.shape)
-    y1 = x1 * cos + x2 * sin # rotate pairs of dims
-    y2 = x1 * (-sin) + x2 * cos
-    out = torch.cat([y1, y2], 3) # re-assemble
-    out = out.to(x.dtype) # ensure input/output dtypes match
-    return out
+# def apply_rotary_emb(x, cos, sin):
+#     assert x.ndim == 4  # multihead attention
+#     d = x.shape[3] // 2
+#     cos = rearrange(cos, 'a b c d -> a c b d')
+#     sin = rearrange(sin, 'a b c d -> a c b d')
+#     x1, x2 = x[..., :d], x[..., d:] # split up last time into two halves
+#     x1, x2 = x1.to(cos.device), x2.to(cos.device)
+#     # print(x1.shape, cos.shape)
+#     y1 = x1 * cos + x2 * sin # rotate pairs of dims
+#     y2 = x1 * (-sin) + x2 * cos
+#     out = torch.cat([y1, y2], 3) # re-assemble
+#     out = out.to(x.dtype) # ensure input/output dtypes match
+#     return out
 
 # def norm(x):
 #     # Purely functional rmsnorm with no learnable params
@@ -62,17 +68,17 @@ def repeat_kv(hidden_states, n_rep):
 
 
 class FastModel:
-    def __init__(self, model_name, rotary_emb: RotaryEmbedding = RotaryEmbedding()) -> None:
+    def __init__(self, model_name) -> None:
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.n_heads = self.model.config.num_attention_heads
         self.n_kv_heads = self.model.config.num_key_value_heads
         self.head_dim = self.model.config.hidden_size // self.n_heads
         self.n_groups = self.n_heads // self.n_kv_heads
-        self.rotary_emb = rotary_emb
-        cos, sin = self.rotary_emb._precompute_rotary_embeddings(seq_len=2048, head_dim=self.head_dim)
-        self.cos = cos
-        self.sin = sin
+        # self.rotary_emb = rotary_emb
+        # cos, sin = self.rotary_emb._precompute_rotary_embeddings(seq_len=2048, head_dim=self.head_dim)
+        # self.cos = cos
+        # self.sin = sin
         
     def decoder_infer(self):
         pass
@@ -86,35 +92,23 @@ class FastModel:
     def attention_fast_forward(self, layer, hidden_states, past_key_value, position_embeddings):
         layer = layer.to('cuda')
         hidden_states = hidden_states.to('cuda')
-        bsz, q_len, _ = hidden_states.size()
+        bsz, q_len, hidden_dim = hidden_states.size()
+        input_shapes = hidden_states.shape[:-1]
+        hidden_shape = (*input_shapes, -1, self.head_dim)
+
         Q, K, V = self.apply_qkv(layer, hidden_states)
         # Shape Q, K, V : (bsz, seq_len, hidden_dim)
-        Q = Q.view(bsz, q_len, self.n_heads, self.head_dim).transpose(1,2)
-        K = K.view(bsz, q_len, self.n_kv_heads, self.head_dim).transpose(1,2)
-        V = V.view(bsz, q_len, self.n_kv_heads, self.head_dim).transpose(1,2)
-
-        kv_seq_len = K.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value[0].shape[-2]
         
-        # print(kv_seq_len)
+        Q = Q.view(hidden_shape).transpose(1,2)
+        K = K.view(hidden_shape).transpose(1,2)
+        V = V.view(hidden_shape).transpose(1,2)
 
         cos, sin = position_embeddings
         
-        Q, K = apply_rotary_emb(Q, cos, sin), apply_rotary_emb(K, cos, sin)
-        # Q, K = norm(Q), norm(K)
-
+        Q, K = apply_rotary_pos_emb(Q, K, cos, sin)
         K = K.to('cuda')
         V = V.to('cuda')
-        if past_key_value is not None:
-            K = torch.cat([past_key_value[0], K], dim=2)
-            V = torch.cat([past_key_value[1], V], dim=2)
-        past_key_value = (K,V)
 
-        K = repeat_kv(K, self.n_groups)  # (bsz, n_heads, kv_seq_len, head_dim)
-        V = repeat_kv(V, self.n_groups)
-        
-        # print(K.shape, V.shape)
         # Flash Attention 
         Q = Q.transpose(1, 2).to('cuda', dtype=torch.bfloat16)
         K = K.transpose(1, 2).to('cuda', dtype=torch.bfloat16)
@@ -146,6 +140,7 @@ class FastModel:
         hidden_states = layer.mlp(hidden_states)
         hidden_states += residual
 
+        layer = layer.to('cpu')
         outputs = (hidden_states,)
         if output_attention:
             outputs += (weights,)
@@ -158,9 +153,15 @@ class FastModel:
         _, seq_len, _ = input_emb.size()
         hidden_states = input_emb
         past_key_value = None
+        
+        hidden_states = hidden_states.to('cuda')
 
-        T0 = 0
-        position_embedding = self.cos[:, T0:T0+seq_len], self.sin[:, T0:T0+seq_len]
+        # T0 = 0
+        position_ids = torch.arange(
+            0, 0 + input_emb.shape[1], device='cuda'
+        ).unsqueeze(0)
+
+        position_embeddings = self.model.model.rotary_emb(hidden_states, position_ids)
 
         hidden_states = hidden_states.to('cuda')
         for layer in self.model.model.layers:
@@ -168,7 +169,7 @@ class FastModel:
                 layer=layer,
                 hidden_states=hidden_states,
                 past_key_value=past_key_value,
-                position_embedding=position_embedding
+                position_embedding=position_embeddings
             )
             hidden_states = outputs[0]
             past_key_value = outputs[-1]
@@ -223,67 +224,56 @@ class FastModel:
         
         print(self.tokenizer.decode(generated_tokens))
         return generated_tokens
-        
 
 def rotate_half(x):
     """Rotates half the hidden dims of the input."""
     x1 = x[..., : x.shape[-1] // 2]
     x2 = x[..., x.shape[-1] // 2 :]
-    return torch.cat((-x2, x1), dim=-1)
-
-# def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
-#     cos = cos.unsqueeze(unsqueeze_dim)
-#     sin = sin.unsqueeze(unsqueeze_dim)
-#     # print(q.shape, k.shape)
-#     # print(cos.shape, sin.shape)
-#     # print(rotate_half(q).shape)
-#     q_embed = (q * cos) + (rotate_half(q) * sin)
-#     k_embed = (k * cos) + (rotate_half(k) * sin)
-#     return q_embed, k_embed
+    return torch.cat((-x2, x1), dim=-1) 
 
 if __name__ == "__main__":
     # model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
     tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
     
-    # dataset = StreamingITDataLoader(ds_name='tatsu-lab/alpaca', tokenizer=tokenizer)
-    # stream_data = dataset._return_stream_ds()
-    # data = None
+    dataset = StreamingITDataLoader(ds_name='tatsu-lab/alpaca', tokenizer=tokenizer)
+    stream_data = dataset._return_stream_ds()
+    data = None
     
-    # for item in stream_data:
-    #     data = dataset.collator(item)
-    #     break
+    for item in stream_data:
+        data = dataset.collator(item)
+        break
     
-    # print(data['input_ids'].shape, data['labels'].shape)
+    print(data['input_ids'].shape, data['labels'].shape)
     
 
     # tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
 
     fast_model = FastModel(model_name='Qwen/Qwen2.5-3B-Instruct')
 
-    test = "Hello, my name is"
-    tokens = tokenizer.encode(test)
-    fast_model.model.to('cuda')
+    # test = "Hello, my name is"
+    # tokens = tokenizer.encode(test)
+    # fast_model.model.to('cuda')
 
-    gen_tokens = fast_model.generate(
-        tokens=tokens,
-        temperature=0.8,
-        max_tokens=20
-    )
+    # gen_tokens = fast_model.generate(
+    #     tokens=tokens,
+    #     temperature=0.8,
+    #     max_tokens=20
+    # )
     
     # print(tokens.shape)
     # # print(model)
     # # token_emb = 
     # # print(model.mode.embed_tokens)
     # rotary_emb = RotaryEmbedding()
-    # final_hidden_states = fast_model.forward(input_ids=data['input_ids'], labels=data['labels'])
+    final_hidden_states = fast_model.forward(input_ids=data['input_ids'], labels=data['labels'])
     # print(final_hidden_states[0].shape)
-    # print(final_hidden_states)
+    print(final_hidden_states)
 
-    model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-3B-Instruct', dtype=torch.bfloat16)
-    model = model.to('cuda')
-    inputs = tokenizer(test, return_tensors='pt').to('cuda')
-    outputs = model.generate(**inputs, max_new_tokens=20)
-    print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+    # model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-3B-Instruct', dtype=torch.bfloat16)
+    # model = model.to('cuda')
+    # # inputs = tokenizer(test, return_tensors='pt').to('cuda')
+    # # outputs = model.generate(**inputs, max_new_tokens=20)
+    # # print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
     # outputs = model.forward(input_ids=data['input_ids'].to('cuda'), labels=data['labels'].to('cuda'))
     # print(outputs)
     # tok_emb = fast_model.model.model.embed_tokens(tokens)
@@ -301,6 +291,9 @@ if __name__ == "__main__":
         # print(len(check))
         # print(check[0].shape)
         # break
+
+    # for name, param in fast_model.model.named_parameters():
+    #     print(name, param.shape, param.requires_grad)
 
     # # fast attention 
     # # print(model.config)
