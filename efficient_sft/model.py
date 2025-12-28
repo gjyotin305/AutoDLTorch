fbd99c1e4287d59d5c6f0bc37e49f1dec5eadc51
gjyotin305 <b22ai063@iitj.ac.in>
2025-12-28 23:57:50 +0530

    Full FT working
    Signed-off-by: gjyotin305 <b22ai063@iitj.ac.in>

--- efficient_sft/model.py
+++ efficient_sft/model.py
@@ -1,7 +1,8 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from einops import rearrange
-from streaming_data import StreamingITDataLoader
 import torch
+import torch.utils.checkpoint as checkpoint
+from unsloth_grad_ckpt import Unsloth_Offloaded_Gradient_Checkpointer
 # from dataclasses import dataclass
 from flash_attn import flash_attn_func
 
@@ -9,29 +10,6 @@ DEVICE_COUNT = 1
 DEVICE_TYPE_TORCH = 'cuda'
 get_current_device = torch.cuda.current_device
 
-# @dataclass
-# class CausalLMStore:
-#     loss: float
-#     logits
-#     tensor
-
-
-# class RotaryEmbedding:
-#     def __init__(self) -> None:
-#         pass
-
-#     def _precompute_rotary_embeddings(self, seq_len, head_dim, base=10000, device=None):
-#         device='cpu'
-#         channel_range = torch.arange(0, head_dim, 2, dtype=torch.float32, device=device)
-#         inv_freq = 1.0 / (base ** (channel_range / head_dim))
-#         # stride the time steps
-#         t = torch.arange(seq_len, dtype=torch.float32, device=device)
-#         # calculate the rotation frequencies at each (time, channel) pair
-#         freqs = torch.outer(t, inv_freq)
-#         cos, sin = freqs.cos(), freqs.sin()
-#         cos, sin = cos.bfloat16(), sin.bfloat16() # keep them in bfloat16
-#         cos, sin = cos[None, :, None, :], sin[None, :, None, :] # add batch and head dims for later broadcasting
-#         return cos, sin
 
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     cos = cos.unsqueeze(unsqueeze_dim)
@@ -40,23 +18,6 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
 
-# def apply_rotary_emb(x, cos, sin):
-#     assert x.ndim == 4  # multihead attention
-#     d = x.shape[3] // 2
-#     cos = rearrange(cos, 'a b c d -> a c b d')
-#     sin = rearrange(sin, 'a b c d -> a c b d')
-#     x1, x2 = x[..., :d], x[..., d:] # split up last time into two halves
-#     x1, x2 = x1.to(cos.device), x2.to(cos.device)
-#     # print(x1.shape, cos.shape)
-#     y1 = x1 * cos + x2 * sin # rotate pairs of dims
-#     y2 = x1 * (-sin) + x2 * cos
-#     out = torch.cat([y1, y2], 3) # re-assemble
-#     out = out.to(x.dtype) # ensure input/output dtypes match
-#     return out
-
-# def norm(x):
-#     # Purely functional rmsnorm with no learnable params
-#     return torch.nn.functional.rms_norm(x, (x.size(-1),))
 
 def repeat_kv(hidden_states, n_rep):
     bsz, n_kv_head, s_len, head_dim = hidden_states.size()
@@ -68,20 +29,15 @@ def repeat_kv(hidden_states, n_rep):
 
 
 class FastModel:
-    def __init__(self, model_name) -> None:
+    def __init__(self, model_name, grad_ckpt: bool = False) -> None:
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = self.model.to('cuda')
+        self.grad_ckpt_check = grad_ckpt
         self.n_heads = self.model.config.num_attention_heads
         self.n_kv_heads = self.model.config.num_key_value_heads
         self.head_dim = self.model.config.hidden_size // self.n_heads
         self.n_groups = self.n_heads // self.n_kv_heads
-        # self.rotary_emb = rotary_emb
-        # cos, sin = self.rotary_emb._precompute_rotary_embeddings(seq_len=2048, head_dim=self.head_dim)
-        # self.cos = cos
-        # self.sin = sin
-        
-    def decoder_infer(self):
-        pass
 
     def apply_qkv(self, layer, hidden_states):
         Q = layer.self_attn.q_proj(hidden_states)
@@ -89,15 +45,12 @@ class FastModel:
         V = layer.self_attn.v_proj(hidden_states)
         return Q, K, V
 
-    def attention_fast_forward(self, layer, hidden_states, past_key_value, position_embeddings):
-        layer = layer.to('cuda')
-        hidden_states = hidden_states.to('cuda')
+    def attention_fast_forward(self, layer, hidden_states, position_embeddings):
         bsz, q_len, hidden_dim = hidden_states.size()
         input_shapes = hidden_states.shape[:-1]
         hidden_shape = (*input_shapes, -1, self.head_dim)
 
         Q, K, V = self.apply_qkv(layer, hidden_states)
-        # Shape Q, K, V : (bsz, seq_len, hidden_dim)
         
         Q = Q.view(hidden_shape).transpose(1,2)
         K = K.view(hidden_shape).transpose(1,2)
@@ -106,8 +59,6 @@ class FastModel:
         cos, sin = position_embeddings
         
         Q, K = apply_rotary_pos_emb(Q, K, cos, sin)
-        K = K.to('cuda')
-        V = V.to('cuda')
 
         # Flash Attention 
         Q = Q.transpose(1, 2).to('cuda', dtype=torch.bfloat16)
@@ -119,73 +70,80 @@ class FastModel:
 
         attn_output = layer.self_attn.o_proj(attn_output.float())
         attn_weights = None
-        return attn_output, attn_weights, past_key_value
+        return attn_output, attn_weights
 
-    def decoder_fast_forward(self, layer, hidden_states, past_key_value, output_attention=True, position_embedding=None): 
-        layer = layer.to('cuda')
+    def decoder_fast_forward(self, hidden_states, layer, position_embedding, output_attention=False): 
         residual = hidden_states
         hidden_states = layer.input_layernorm(hidden_states)
-        hidden_states, weights, present_key_value = self.attention_fast_forward(
+        
+        hidden_states, _ = self.attention_fast_forward(
             layer=layer,
             hidden_states=hidden_states,
-            past_key_value=past_key_value,
             position_embeddings=position_embedding
         )
-        # hidden_states = hidden_states.to()
-        # residual = residual.to('cuda')
-        hidden_states += residual.to('cuda')
+        hidden_states += residual
 
         residual = hidden_states
         hidden_states = layer.post_attention_layernorm(hidden_states)
         hidden_states = layer.mlp(hidden_states)
         hidden_states += residual
 
-        layer = layer.to('cpu')
-        outputs = (hidden_states,)
-        if output_attention:
-            outputs += (weights,)
-            outputs += (present_key_value,)
+        outputs = hidden_states
+        # if output_attention:
+        #     outputs += (weights.detach(),)
 
         return outputs
 
-    def forward(self, input_ids, labels):
-        input_emb = self.model.model.embed_tokens(input_ids)
+    def decoder_forward_ckpt(self, *args, **kwargs):
+        def forward_fn(*inputs):
+            return self.decoder_fast_forward(*inputs, **kwargs)
+        return checkpoint.checkpoint(forward_fn, *args, use_reentrant=False)
+
+    def forward(self, input_ids, labels=None):
+        input_emb = self.model.model.embed_tokens(input_ids.to('cuda'))
         _, seq_len, _ = input_emb.size()
         hidden_states = input_emb
-        past_key_value = None
         
-        hidden_states = hidden_states.to('cuda')
-
-        # T0 = 0
         position_ids = torch.arange(
-            0, 0 + input_emb.shape[1], device='cuda'
+            0, 0 + seq_len, device='cuda'
         ).unsqueeze(0)
 
         position_embeddings = self.model.model.rotary_emb(hidden_states, position_ids)
 
-        hidden_states = hidden_states.to('cuda')
         for layer in self.model.model.layers:
-            outputs = self.decoder_fast_forward(
-                layer=layer,
-                hidden_states=hidden_states,
-                past_key_value=past_key_value,
-                position_embedding=position_embeddings
+            def make_decoder_fn(layer):
+                def fn(hidden_states, position_embeddings):
+                    return self.decoder_fast_forward(
+                        layer=layer,
+                        hidden_states=hidden_states,
+                        position_embedding=position_embeddings
+                    )
+                return fn
+            fn = make_decoder_fn(layer)
+            # outputs = checkpoint.checkpoint(
+            #     self.decoder_fast_forward,
+            #     layer=layer,
+            #     hidden_states=hidden_states,
+            #     position_embedding=position_embeddings,
+            #     use_reentrant=False
+            # )
+            # hidden_states = outputs[0]
+            # else:
+            outputs = Unsloth_Offloaded_Gradient_Checkpointer.apply(
+                fn,
+                hidden_states,
+                position_embeddings
             )
-            hidden_states = outputs[0]
-            past_key_value = outputs[-1]
+            hidden_states = outputs
             
-        self.model.lm_head = self.model.lm_head.to('cuda')
-        self.model.model.norm = self.model.model.norm.to('cuda') 
+        # Pre LM Head Layer Norm 
         hidden_states = self.model.model.norm(hidden_states)
         logits = self.model.lm_head(hidden_states)
         loss = None
         
-        # softcap=15
         if labels is not None:
-            labels = labels.to('cuda')
             logits = logits[..., :-1, :].contiguous()
             labels = labels[..., 1:].contiguous()
-            # logits = softcap * torch.tanh(logits / softcap)
             logits = logits.float()
             loss = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), labels.view(-1), reduction='mean', ignore_index=-100)
         
@@ -193,14 +151,13 @@ class FastModel:
             'loss': loss,
             'logits': logits,
             'hidden_states': hidden_states,
-            'past_key_value': past_key_value
         }
 
     def generate(
         self,
         tokens,
         temperature: float = 0.0,
-        max_tokens: int = 128
+        max_tokens: int = 128,
     ):  
         generated_tokens = []
         rng = None
@@ -220,6 +177,9 @@ class FastModel:
                 next_ids = torch.argmax(logits, dim=-1, keepdim=True)
             ids = torch.cat((ids, next_ids), dim=1)
             token = next_ids.item()
+            if token == 151645:
+                break
+            # print(self.tokenizer.decode([token]))
             generated_tokens.append(token)
         
         print(self.tokenizer.decode(generated_tokens))
@@ -231,118 +191,7 @@ def rotate_half(x):
     x2 = x[..., x.shape[-1] // 2 :]
     return torch.cat((-x2, x1), dim=-1) 
 
-if __name__ == "__main__":
-    # model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
-    tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
-    
-    dataset = StreamingITDataLoader(ds_name='tatsu-lab/alpaca', tokenizer=tokenizer)
-    stream_data = dataset._return_stream_ds()
-    data = None
-    
-    for item in stream_data:
-        data = dataset.collator(item)
-        break
-    
-    print(data['input_ids'].shape, data['labels'].shape)
-    
-
-    # tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-3B-Instruct')
-
-    fast_model = FastModel(model_name='Qwen/Qwen2.5-3B-Instruct')
-
-    # test = "Hello, my name is"
-    # tokens = tokenizer.encode(test)
-    # fast_model.model.to('cuda')
-
-    # gen_tokens = fast_model.generate(
-    #     tokens=tokens,
-    #     temperature=0.8,
-    #     max_tokens=20
-    # )
-    
-    # print(tokens.shape)
-    # # print(model)
-    # # token_emb = 
-    # # print(model.mode.embed_tokens)
-    # rotary_emb = RotaryEmbedding()
-    final_hidden_states = fast_model.forward(input_ids=data['input_ids'], labels=data['labels'])
-    # print(final_hidden_states[0].shape)
-    print(final_hidden_states)
-
-    # model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-3B-Instruct', dtype=torch.bfloat16)
-    # model = model.to('cuda')
-    # # inputs = tokenizer(test, return_tensors='pt').to('cuda')
-    # # outputs = model.generate(**inputs, max_new_tokens=20)
-    # # print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
-    # outputs = model.forward(input_ids=data['input_ids'].to('cuda'), labels=data['labels'].to('cuda'))
-    # print(outputs)
-    # tok_emb = fast_model.model.model.embed_tokens(tokens)
-    # print(tok_emb.shape)
-    # print(fast_model.model)
-    # for layer in fast_model.model.model.layers:
-    #     print(layer)
-        # check = fast_model.decoder_fast_forward(
-        #     layer=layer,
-        #     hidden_states=tok_emb,
-        #     past_key_value=None,
-        #     position_embedding=None
-        # )
-        # print(check)
-        # print(len(check))
-        # print(check[0].shape)
-        # break
-
-    # for name, param in fast_model.model.named_parameters():
-    #     print(name, param.shape, param.requires_grad)
-
-    # # fast attention 
-    # # print(model.config)
-    # n_heads = model.config.num_attention_heads
-    # n_kv_heads = model.config.num_key_value_heads
-    # head_dim = model.config.hidden_size // n_heads
-    # n_groups = n_heads // n_kv_heads
-    # rotary_emb = RotaryEmbedding()
-    # use_cache = False
-
-    # for layer in model.model.layers:
-    #     print(layer)
-    #     bsz, q_len, _ = tok_emb.size()
-    #     Q, K, V = layer.self_attn.q_proj(tok_emb), layer.self_attn.k_proj(tok_emb), layer.self_attn.v_proj(tok_emb)
-    #     Q = Q.view(bsz, q_len, n_heads, head_dim).transpose(1,2)
-    #     K = K.view(bsz, q_len, n_kv_heads, head_dim).transpose(1,2)
-    #     V = V.view(bsz, q_len, n_kv_heads, head_dim).transpose(1,2)
-    #     print(Q.shape, K.shape, V.shape)
-    #     kv_seq_len = K.shape[-2]
-        # cos, sin = rotary_emb._precompute_rotary_embeddings(seq_len=kv_seq_len, head_dim=128)
-    #     # rotary_emb.extend_rope_embedding(x=V, seq_len=kv_seq_len)
-    #     # cos, sin = rotary_emb.get_cached(kv_seq_len, Q.device.index)
-    #     # print(cos.shape)
-    #     Q, K = apply_rotary_emb(Q, cos, sin), apply_rotary_emb(K, cos, sin)
-    #     past_key_value = (K, V) if use_cache else None
-    #     print(K.shape, Q.shape)
-    #     Q = Q.transpose(1, 2)
-    #     K = K.transpose(1, 2)
-    #     V = V.transpose(1, 2)
-    #     Q = Q.to('cuda').bfloat16()
-    #     K = K.to('cuda').bfloat16()
-    #     V = V.to('cuda').bfloat16()
-    #     A = flash_attn_func(Q, K, V, causal = True)
-    #     # print(A.shape)
-    #     attn_output = A.reshape(bsz, q_len, n_heads * head_dim)
-    #     print(attn_output.shape)
-    #     print(attn_output.dtype)
-    #     print(attn_output)
-    #     # print(la)
-    #     # layer.self_attn.o_proj.to('cuda')
-    #     attn_output = attn_output.to('cpu').float()
-    #     attn_output = layer.self_attn.o_proj(attn_output)
-    
-    #     print(attn_output.shape, past_key_value)
-    #     break   
-
-    # print(tok_emb, tok_emb.shape)
-    # for name, module in model.named_modules():
-        # print(f"Name: {name}, Module: {module}")
+# if __name__ == "__main__":
 
 
 
---
+++ efficient_sft/train.py
@@ -0,0 +1,126 @@
+import torch
+import wandb
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from model import FastModel
+from tqdm import tqdm
+from streaming_data import StreamingITDataLoader
+
+config = {
+    'lr':5e-5,
+    'model': 'Full Finetune',
+    'dataset': 'Alpaca',
+    'steps_epoch': 200,
+    'epochs': 3,
+    'grad_accm': 4
+}
+
+@torch.no_grad()
+def gradient_norm(params):
+    """Computes the 2-norm of the gradients of the given parameters."""
+    total = torch.tensor(0.0)
+    for p in params:
+        if p.grad is not None:
+            total = total.to(p.grad.device)
+            total += torch.norm(p.grad, dtype=total.dtype) ** 2
+    return torch.sqrt(total)
+
+def setup_optimizer_transformer_model(model, config):
+    # decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+    # nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+    matrix_params = list(model.model.layers.parameters())
+    embedding_params = list(model.model.embed_tokens.parameters())
+    lm_head_params = list(model.lm_head.parameters())
+    
+    assert len(list(model.parameters())) == len(matrix_params) + len(embedding_params) + len(lm_head_params)
+
+    adam_groups = [
+        dict(params=lm_head_params, lr=2e-4),
+        dict(params=matrix_params, lr=2e-5)
+    ]
+
+    optimizer = torch.optim.AdamW(adam_groups)
+    return optimizer
+
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-1.5B-Instruct')
+    dataset = StreamingITDataLoader(ds_name='tatsu-lab/alpaca', tokenizer=tokenizer)
+    stream_data = dataset._return_stream_ds()
+    
+    grad_accm_steps = config['grad_accm']
+    
+    fast = FastModel(model_name='Qwen/Qwen2.5-1.5B-Instruct', grad_ckpt=True)
+    fast.model.train(True)
+    
+    wandb.init(
+        project='efficient-sft',
+        config=config
+    )
+
+    for p in fast.model.parameters():
+        p.requires_grad_(True)
+    
+    optimizer = setup_optimizer_transformer_model(fast.model, config)
+    # scheduler = torch.optim.lr_scheduler.LinearLR(
+    #     optimizer,
+    #     start_factor=1.0,
+    #     end_factor=0.0,
+    #     total_iters=config['total_steps']
+    # )
+    total_steps = config['epochs'] * config['steps_epoch'] // config['grad_accm']
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer,
+        T_max=total_steps,
+    )
+    
+    pbar = tqdm(total=total_steps)
+    for epoch in range(config['epochs']):
+        for idx, item in enumerate(stream_data):
+            data = dataset.collator(item)
+            
+            input_ids = data['input_ids'].to('cuda')
+            labels = data['labels'].to('cuda')
+            
+            out = fast.forward(input_ids=input_ids, labels=labels)
+            loss = out['loss'] / grad_accm_steps  
+            loss.backward()  
+
+            if (idx + 1) % grad_accm_steps == 0:
+                pre_clip = gradient_norm(fast.model.parameters()) 
+                torch.nn.utils.clip_grad_norm_(fast.model.parameters(), max_norm=1.0)
+                post_clip = gradient_norm(fast.model.parameters())
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad(set_to_none=True)
+                pbar.set_postfix(
+                    {"epoch": epoch, "step": idx//grad_accm_steps, "loss": loss.item()*grad_accm_steps, "pre_grad_norm": pre_clip.item(), "post_grad_norm": post_clip.item()}
+                )
+                wandb.log(
+                    {
+                        "loss/train": loss.item(),
+                        "lr": optimizer.param_groups[0]["lr"],
+                        "gradient_norm/train": post_clip.item()
+                    }
+                )
+                pbar.update(1)
+            
+            if (idx+1) % 50 == 0:
+                with torch.no_grad():
+                    messages = [
+                        {"role": "system", "content": "You are an instruction following agent.",},
+                        {"role": "user", "content": "## Instruction: Give three tips for staying healthy.\n ## Input: "},
+                    ]
+
+                    tokens = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True)
+
+                    _ = fast.generate(
+                        tokens=tokens,
+                        temperature=0,
+                        max_tokens=30
+                    )
+                
+
+            if (idx+1) == config['steps_epoch']:
+                break   
+    
+    wandb.finish()
+    print('Run finished')
